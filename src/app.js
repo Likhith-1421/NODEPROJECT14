fe9288d944f35da2ca39a851f4bdea81a43cb403
Likhith <likhithpbk@gmail.com>
2025-12-15 11:27:47 +0530

    Ignore node_modules

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+app.use("/home",(req,res)=>{
+    res.send("HELLO SERVER HEllo")
+})
+app.use("/text",(req,res)=>{
+    res.send("HELLO SERVER LIKHITH")
+})
+
+app.listen(7777,()=>{
+    console.log('CONNECTED TO SERVER !')
+})
+app.listen(8888,()=>{
+    console.log('CONNECTED TO SERVER !')
+})
