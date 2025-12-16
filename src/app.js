2691adf938a657247a840df38fb67e50641ab16e
Likhith <likhithpbk@gmail.com>
2025-12-16 12:08:56 +0530

    playing with route handlers

--- src/app.js
+++ src/app.js
@@ -3,27 +3,37 @@ const express = require('express')
 const app = express()
 
 
-app.use("/home/3",(req,res)=>{
-    res.send("HELLO SERVER 3")
+// app.use("/home/3",(req,res)=>{
+//     res.send("HELLO SERVER 3")
+// })
+app.use("/testing",(req,res,next)=>{
+    console.log("route 1")
+    next()
+},
+(req,res,next)=>{
+ console.log('route 2')
+ next()
+},
+(req,res,next)=>{
+   res.send("HELLO EVERY ONE!!")    
 })
+app.listen(8888,()=>{
+    console.log('CONNECTED TO SERVER !')
+})
+
+
+
+
+
+
+
+
+
+
+
 // app.use("/home",(req,res)=>{
 //     res.send("HELLO SERVER HEllo")
 // })
 // app.use("/text",(req,res)=>{
 //     res.send("HELLO SERVER LIKHITH")
 // })
-
-app.post("/user/:userID/:password",(req,res)=>{
-    console.log(req.params)
-    res.send({"firstname":"LIKHITH", "secondname":"puttabakula"})
-})
-
-app.listen(7777,()=>{
-    console.log('CONNECTED TO SERVER !')
-})
-app.listen(8888,()=>{
-    console.log('CONNECTED TO SERVER !')
-})
-app.use("/",(req,res)=>{
-    res.send("hello /")
-})
