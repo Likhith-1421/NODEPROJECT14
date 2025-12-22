428118582962c5f8fb8e73709c161f0966ccafb0
Likhith <likhithpbk@gmail.com>
2025-12-22 10:08:31 +0530

    PLAYING AND CREATING WITH APIs

--- src/app.js
+++ src/app.js
@@ -3,56 +3,73 @@ const database = require("./config/database")
 const User = require("./model/UserSchema")
 const app = express()
 
+
 app.use(express.json())
-app.post("/signup", async(req,res)=>{
+app.post("/signup",async(req,res)=>{
+    const user = User(req.body);
+    console.log(user)
+    
+          await user.save()
+          res.send("CHECK DATA IN DATA BASE")
+    
+   
+});
 
-  
 
-    const user = User(req.body)
-
+app.get("/findall",async(req,res)=>{
     try{
-    await user.save()
-    res.send("register completed ")
+       const result = await User.find({})
+       res.send(result)
     }
     catch(err)
     {
-        res.status(400).send("something went wrong")
-    }
-
-
+        res.status(404).send("DATA NOT FOUND")
    
+    }
+});
+
+app.get("/find",async(req,res)=>{
+   const user =  req.body.firstName
+  const result = await User.find({firstName:user})
+  try{
+       res.send(result)
+  }
+  catch(err)
+  {
+      res.status(404).send("DATA NOT FOUND")
+  }
 })
 
+app.get("/findone",async(req,res)=>{
+   const user = req.body.firstName
+   const result = await User.findOne({firstName:user})
+   try{
+         res.send(result)
+   }
+   catch(err)
+   {
+        res.status(404).send("DATA NOT FOUND")
+   }
+})
+
+app.get("/delete",async(req,res)=>{
+   const user =  req.body.lastName
+   const result = await User.deleteOne({lastName:user})
+   try{
+      res.send(result)
+   }
+   catch(err)
+   {
+      res.status(404).send("WE ARE SORRY")
+   }
+})
 
 database()
 .then(()=>{
     console.log("DATA BASE CONNECTED SUCCESFULLY")
     app.listen(8888,()=>{console.log("connected to port 8888")})
-})
+});
 
-// const {admin} = require("./middleware/auth")
-// app.use("/auth", admin)
-
-// // app.get("/auth/corret",(req,res)=>
-// // {
-// //     res.send("HELLO LIKHITH")
-// // })
-// app.use("/error",(req,res)=>{
-//    throw new error("hufuydvcvb");
-   
-// })
-
-// app.use("/",(err,req,res,next)=>
-// {
-//   if(err)
-//     {
-//         res.status(500).send("Something went wrong!!")
-//     }
-// })
-
-// app.listen(8888,()=>{
-//     console.log('CONNECTED TO SERVER !')
-// })
 
 
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose")
-
-const database= async()=>
-    {mongoose.connect("mongodb+srv://likhithpbk_db_user:NODEPROJECTMAIN@nodeprojectmain.nzmjghj.mongodb.net/")}
-module.exports = database
+const mongoose = require ("mongoose")
+const database = async()=>{
+   await mongoose.connect("mongodb+srv://likhithpbk_db_user:NODEPROJECTMAIN@nodeprojectmain.nzmjghj.mongodb.net/")
+}
+module.exports = database;
